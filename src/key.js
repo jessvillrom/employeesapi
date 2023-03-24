32743d78f43d86b5c5c64c35c7a07d40b0bb83a6
Camilo <gsusvillamil@gmail.com>
2023-03-23 23:57:12 -0500

    SET Config Desploy DB

--- src/key.js
+++ src/key.js
@@ -1,8 +1,17 @@
+import {
+  DB_PORT,
+  DB_DATABASE,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+} from "./config.js";
+
 export default {
   database: {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "companydb",
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    port: DB_PORT,
   },
 };
